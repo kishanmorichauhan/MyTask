7b140dac94347ea762b34200faff421a597b4c21
kishan morichauhan <kishanmorichauhan@gmail.com>
2021-06-06 11:59:53 +0530

    -

---
+++ app/src/main/java/com/example/task8/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.task8;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Fragment fragment = new MapFragment();
+
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.frame_layout,fragment)
+                .commit();
+    }
+}
